e25622c9f08aa30673fed1a4ded7ccb346a36286
icntdthsnmr <BlackAr04@gmail.com>
2023-05-16 00:45:25 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+def z1():
+    class Restaurant:
+
+        def __init__(self, restaurant_name, cuisine_type):
+            self.restaurant_name = restaurant_name
+            self.cuisine_type = cuisine_type
+
+        def describe_restaurant(self):
+            print(f"Название ресторана: {self.restaurant_name}")
+            print(f"Тип кухни: {self.cuisine_type}")
+
+        def open_restaurant(self):
+            print("Ресторан открыт!")
+
+
+    newRestaurant = Restaurant("Persona Grata", "Европейская")
+
+    print(newRestaurant.restaurant_name)
+    print(newRestaurant.cuisine_type)
+    print()
+    newRestaurant.describe_restaurant()
+    newRestaurant.open_restaurant()
+
+def z2():
+    class Restaurant:
+        def __init__(self, restaurant_name, cuisine_type):
+            self.restaurant_name = restaurant_name
+            self.cuisine_type = cuisine_type
+
+        def describe_restaurant(self):
+            print(f"Название ресторана: {self.restaurant_name}")
+            print(f"Тип кухни: {self.cuisine_type}")
+
+
+    res_1 = Restaurant("Вкусно и точка", "Фаст-фуд")
+    res_2 = Restaurant("Кимчи", "Корейская")
+    res_3 = Restaurant("Такояки", "Японская")
+
+    res_1.describe_restaurant()
+    print()
+    res_2.describe_restaurant()
+    print()
+    res_3.describe_restaurant()
+
+def z3():
+    class Restaurant:
+        def __init__(self, restaurant_name, cuisine_type, rating=0):
+            self.restaurant_name = restaurant_name
+            self.cuisine_type = cuisine_type
+            self.rating = rating
+
+        def describe_restaurant(self):
+            print(f"Название ресторана: {self.restaurant_name}")
+            print(f"Тип кухни: {self.cuisine_type}")
+
+        def open_restaurant(self):
+            print(f"Ресторан открыт!")
+
+        def old_rating(self, rating):
+            self.rating = rating
+            print(f"Рейтинг: {self.rating}")
+
+        def new_rating(self, new_rat):
+            self.rating = float(input('Введите новое значение рейтинга: '))
+            print()
+            newRestaurant = Restaurant("Persona Grata", "Европейская")
+            newRestaurant.describe_restaurant()
+            newRestaurant.open_restaurant()
+            print(f"Рейтинг обновлён: {self.rating}")
+
+    newRestaurant = Restaurant("Persona Grata", "Европейская")
+    newRestaurant.describe_restaurant()
+    newRestaurant.open_restaurant()
+    newRestaurant.old_rating(3.5)
+    print()
+    newRestaurant.new_rating(3)
+
